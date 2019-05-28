1bebcbf938eb01bf18815d49385303ee22f95631
Kameron Kenny <kameron.kenny@salesforce.com>
2019-05-28 14:10:36 -0400

    update events

--- broadcast_timer/events.js
+++ broadcast_timer/events.js
@@ -5,18 +5,68 @@ var events = [
     "notes": ""
   },
   {
-    "date": "May 24, 2019 15:00:00",
-    "subject": "Example 1",
-    "notes": "EXMAMPLE 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur purus ut faucibus pulvinar elementum. Pellentesque habitant morbi tristique senectus et. Amet risus nullam eget felis. Semper auctor neque vitae tempus quam pellentesque nec nam aliquam."
+    "date": "May 26, 2019 07:00:00",
+    "subject": "VTO",
+    "notes": "at Crosspoint"
   },
   {
-    "date": "May 24, 2019 15:33:00",
-    "subject": "Example 2",
-    "notes": "EXAMPLE 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur purus ut faucibus pulvinar elementum. Pellentesque habitant morbi tristique senectus et. Amet risus nullam eget felis. Semper auctor neque vitae tempus quam pellentesque nec nam aliquam."
+    "date": "May 26, 2019 13:00:00",
+    "subject": "Lunch",
+    "notes": "Something good."
   },
   {
-    "date": "May 24, 2020 15:33:00",
-    "subject": "END",
+    "date": "May 27, 2019 00:00:01",
+    "subject": "Memorial Day",
+    "notes": ""
+  },
+  {
+    "date": "May 28, 2019 09:00:00",
+    "subject": "1:1 A",
+    "notes": ""
+  },
+  {
+    "date": "May 28, 2019 09:30:00",
+    "subject": "Team Meeting",
+    "notes": ""
+  },
+  {
+    "date": "May 28, 2019 10:00:00",
+    "subject": "Code Review",
+    "notes": ""
+  },
+  {
+    "date": "May 28, 2019 11:15:00",
+    "subject": "DSU",
+    "notes": ""
+  },
+  {
+    "date": "May 28, 2019 14:00:00",
+    "subject": "MTA Tool Deploy Meeting",
+    "notes": ""
+  },
+  {
+    "date": "May 28, 2019 15:30:00",
+    "subject": "OS Review Board",
+    "notes": ""
+  },
+  {
+    "date": "May 28, 2019 18:30:00",
+    "subject": "1:1 S",
+    "notes": ""
+  },
+  {
+    "date": "May 28, 2019 19:00:00",
+    "subject": "AU Team Meeting",
+    "notes": ""
+  },
+  {
+    "date": "May 28, 2019 19:00:00",
+    "subject": "1:1 P",
+    "notes": ""
+  },
+  {
+    "date": "May 29, 2019 09:30:00",
+    "subject": "1:1 JB",
     "notes": ""
   }
 
