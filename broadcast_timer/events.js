a1fa4bfbe481d16486a02b0b145622d0ec2b2153
Kameron Kenny <kameron.kenny@salesforce.com>
2019-05-28 14:29:57 -0400

    updates

--- broadcast_timer/events.js
+++ broadcast_timer/events.js
@@ -40,9 +40,14 @@ var events = [
     "notes": ""
   },
   {
-    "date": "May 28, 2019 14:00:00",
+    "date": "May 28, 2019 14:30:00",
     "subject": "MTA Tool Deploy Meeting",
-    "notes": ""
+    "notes": "Description:discuss deployment of tools -- Chef, something else?<br />discuss updates to sudoers file to allow headless (no login) service account to run specific executable in /sbin/ directory as in operator ALL= /sbin/poweroff <br /> ## example from: https://www.garron.me/en/linux/visudo-command-sudoers-file-sudo-default-editor.html"
+  },
+  {
+    "date": "May 28, 2019 15:00:00",
+    "subject": "Update ORB Notes",
+    "notes": "1. Changing the audience to be manager and PO and removing the engineers from the discussion. <br /><br />2. Please use this link for all things MC-ORB<br /><br />3. Please fill out the <a href=https://docs.google.com/document/d/1S3LeKT9wqSM1EUL4U6h-yJkjEj3iy8xHFAgERgTnC1A/edit#>MC-ORB Notes Document</a> to contain your teams updates PRIOR to the meeting so that we can quickly get through the cadence and end the meeting early.  Ideally this meeting should never last 30 minutes."
   },
   {
     "date": "May 28, 2019 15:30:00",
