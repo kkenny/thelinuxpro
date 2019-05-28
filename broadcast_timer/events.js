847e2aab02ee30d7288f065b08de9f8d897b19ad
Kameron Kenny <kameron.kenny@salesforce.com>
2019-05-28 15:23:01 -0400

    update events

--- broadcast_timer/events.js
+++ broadcast_timer/events.js
@@ -71,8 +71,82 @@ var events = [
   },
   {
     "date": "May 29, 2019 09:30:00",
-    "subject": "1:1 JB",
+    "subject": "1:1",
+    "notes": ""
+  },
+  {
+    "date": "May 29, 2019 10:30:00",
+    "subject": "221 Release Plan Retro",
+    "notes": "<ul><li>Review the 221 Release Planning cycle</li><li>Identify improvements for future planning cycles</li><li>Vote on 1-2 improvements for 222.</li></ul><br />With the 222 planning cycle about to begin, I wanted to be sure to set up a retro on 221 planning. As with everything else Agile, the goal is to continually inspect, adapt and iteratively improve!<br /><br />Since I expect some folks to have conflicts, be on the lookout for a link to a Google Jamboard where you can add your items prior to this meeting."
+  },
+  {
+    "date": "May 29, 2019 11:00:00",
+    "subject": "XLT Meeting",
+    "notes": "Lunch Provided"
+  },
+  {
+    "date": "May 29, 2019 11:30:00",
+    "subject": "DPS LT",
+    "notes": "Lunch Provided"
+  },
+  {
+    "date": "May 29, 2019 15:00:00",
+    "subject": "Monit w/ Gumby",
+    "notes": "Demo Monit and how it can be useful for gumby"
+  },
+  {
+    "date": "May 30, 2019 00:00:01",
+    "subject": "10-year anniversery",
+    "notes": ""
+  },
+  {
+    "date": "May 30, 2019 07:40:00",
+    "subject": "Bus",
+    "notes": ""
+  },
+  {
+    "date": "May 30, 2019 12:00:00",
+    "subject": "Lunch w/ Brad",
+    "notes": ""
+  },
+  {
+    "date": "May 30, 2019 16:00:00",
+    "subject": "Bus",
+    "notes": ""
+  },
+  {
+    "date": "May 31, 2019 09:00:00",
+    "subject": "1:1",
+    "notes": ""
+  },
+  {
+    "date": "May 31, 2019 09:30:00",
+    "subject": "Linux Build Handoff",
+    "notes": ""
+  },
+  {
+    "date": "May 31, 2019 10:00:00",
+    "subject": "Problem Review Session",
+    "notes": ""
+  },
+  {
+    "date": "May 31, 2019 11:15:00",
+    "subject": "DSU",
+    "notes": ""
+  },
+  {
+    "date": "May 31, 2019 11:30:00",
+    "subject": "Retro",
+    "notes": ""
+  },
+  {
+    "date": "May 31, 2019 12:00:00",
+    "subject": "M&J Func Leadership Monthly",
+    "notes": ""
+  },
+  {
+    "date": "May 31, 2019 14:00:00",
+    "subject": "1:1",
     "notes": ""
   }
-
 ]
