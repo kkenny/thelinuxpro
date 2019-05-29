6a5b10f823d45e82ef201570f7dd2ffd5928d692
Kameron Kenny <kameron.kenny@salesforce.com>
2019-05-29 17:37:33 -0400

    event update

--- broadcast_timer/events.js
+++ broadcast_timer/events.js
@@ -1,70 +1,5 @@
 var events = [
   {
-    "date": "May 28, 2019 09:30:00",
-    "subject": "Team Meeting",
-    "notes": ""
-  },
-  {
-    "date": "May 28, 2019 10:00:00",
-    "subject": "Code Review",
-    "notes": ""
-  },
-  {
-    "date": "May 28, 2019 11:15:00",
-    "subject": "DSU",
-    "notes": ""
-  },
-  {
-    "date": "May 28, 2019 14:30:00",
-    "subject": "MTA Tool Deploy Meeting",
-    "notes": "Description:discuss deployment of tools -- Chef, something else?<br />discuss updates to sudoers file to allow headless (no login) service account to run specific executable in /sbin/ directory as in operator ALL= /sbin/poweroff <br /> ## example from: https://www.garron.me/en/linux/visudo-command-sudoers-file-sudo-default-editor.html"
-  },
-  {
-    "date": "May 28, 2019 15:00:00",
-    "subject": "Update ORB Notes",
-    "notes": "1. Changing the audience to be manager and PO and removing the engineers from the discussion. <br /><br />2. Please use this link for all things MC-ORB<br /><br />3. Please fill out the <a href=https://docs.google.com/document/d/1S3LeKT9wqSM1EUL4U6h-yJkjEj3iy8xHFAgERgTnC1A/edit#>MC-ORB Notes Document</a> to contain your teams updates PRIOR to the meeting so that we can quickly get through the cadence and end the meeting early.  Ideally this meeting should never last 30 minutes."
-  },
-  {
-    "date": "May 28, 2019 15:30:00",
-    "subject": "OS Review Board",
-    "notes": ""
-  },
-  {
-    "date": "May 28, 2019 18:30:00",
-    "subject": "1:1 S",
-    "notes": ""
-  },
-  {
-    "date": "May 28, 2019 19:00:00",
-    "subject": "AU Team Meeting",
-    "notes": ""
-  },
-  {
-    "date": "May 28, 2019 19:00:00",
-    "subject": "1:1 P",
-    "notes": ""
-  },
-  {
-    "date": "May 29, 2019 09:30:00",
-    "subject": "1:1",
-    "notes": ""
-  },
-  {
-    "date": "May 29, 2019 10:30:00",
-    "subject": "221 Release Plan Retro",
-    "notes": "<ul><li>Review the 221 Release Planning cycle</li><li>Identify improvements for future planning cycles</li><li>Vote on 1-2 improvements for 222.</li></ul><br />With the 222 planning cycle about to begin, I wanted to be sure to set up a retro on 221 planning. As with everything else Agile, the goal is to continually inspect, adapt and iteratively improve!<br /><br />Since I expect some folks to have conflicts, be on the lookout for a link to a Google Jamboard where you can add your items prior to this meeting."
-  },
-  {
-    "date": "May 29, 2019 11:00:00",
-    "subject": "XLT Meeting",
-    "notes": "Lunch Provided"
-  },
-  {
-    "date": "May 29, 2019 11:30:00",
-    "subject": "DPS LT",
-    "notes": "Lunch Provided"
-  },
-  {
     "date": "May 29, 2019 15:00:00",
     "subject": "Monit w/ Gumby",
     "notes": "Demo Monit and how it can be useful for gumby"
@@ -75,13 +10,33 @@ var events = [
     "notes": ""
   },
   {
+    "date": "May 29, 2019 17:40:00",
+    "subject": "Go Home",
+    "notes": ""
+  },
+  {
     "date": "May 30, 2019 00:00:01",
     "subject": "10-year anniversery",
     "notes": ""
   },
   {
+    "date": "May 30, 2019 08:00:00",
+    "subject": "Start Day",
+    "notes": ""
+  },
+  {
     "date": "May 30, 2019 07:40:00",
     "subject": "Bus",
+    "notes": ""
+  },
+  {
+    "date": "May 30, 2019 09:00:00",
+    "subject": "Check on Lunch w/ Brad",
+    "notes": "Find out where to go"
+  },
+  {
+    "date": "May 30, 2019 11:45:00",
+    "subject": "Leave for Lunch",
     "notes": ""
   },
   {
