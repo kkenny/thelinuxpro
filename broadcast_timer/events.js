918c3c3d75ddf83f9e474ef95aef8b67e56866c4
Kameron Kenny <kameron.kenny@salesforce.com>
2019-06-03 08:31:21 -0400

    events update

--- broadcast_timer/events.js
+++ broadcast_timer/events.js
@@ -1,79 +1,4 @@
 var events = [
-  {
-    "date": "May 29, 2019 15:00:00",
-    "subject": "Monit w/ Gumby",
-    "notes": "Demo Monit and how it can be useful for gumby"
-  },
-  {
-    "date": "May 29, 2019 16:25:00",
-    "subject": "Git Push",
-    "notes": ""
-  },
-  {
-    "date": "May 29, 2019 17:40:00",
-    "subject": "Go Home",
-    "notes": ""
-  },
-  {
-    "date": "May 30, 2019 00:00:01",
-    "subject": "10-year anniversery",
-    "notes": ""
-  },
-  {
-    "date": "May 30, 2019 08:00:00",
-    "subject": "Start Day",
-    "notes": ""
-  },
-  {
-    "date": "May 30, 2019 07:40:00",
-    "subject": "Bus",
-    "notes": ""
-  },
-  {
-    "date": "May 30, 2019 09:00:00",
-    "subject": "Check on Lunch w/ Brad",
-    "notes": "Find out where to go"
-  },
-  {
-    "date": "May 30, 2019 16:00:00",
-    "subject": "Bus",
-    "notes": ""
-  },
-  {
-    "date": "May 31, 2019 09:00:00",
-    "subject": "1:1",
-    "notes": ""
-  },
-  {
-    "date": "May 31, 2019 09:30:00",
-    "subject": "Linux Build Handoff",
-    "notes": ""
-  },
-  {
-    "date": "May 31, 2019 10:00:00",
-    "subject": "Problem Review Session",
-    "notes": ""
-  },
-  {
-    "date": "May 31, 2019 11:15:00",
-    "subject": "DSU",
-    "notes": ""
-  },
-  {
-    "date": "May 31, 2019 11:30:00",
-    "subject": "Retro",
-    "notes": ""
-  },
-  {
-    "date": "May 31, 2019 12:00:00",
-    "subject": "M&J Func Leadership Monthly",
-    "notes": ""
-  },
-  {
-    "date": "May 31, 2019 14:00:00",
-    "subject": "1:1",
-    "notes": ""
-  },
 	{
 		"date": "May 30, 2019 10:00:00",
 		"subject": "Create TD for Splunk Forwarder",
@@ -102,6 +27,27 @@ var events = [
 	{
 		"date": "May 30, 2019 13:30:00",
 		"subject": "Review Epics on Release",
+		"notes": ""
+	},
+// Jun 3
+	{
+		"date": "June 03, 2019 08:30:00",
+		"subject": "Linux Lottery Scenario Run-through",
+		"notes": ""
+	},
+	{
+		"date": "June 03, 2019 11:15:00",
+		"subject": "DSU",
+		"notes": ""
+	},
+	{
+		"date": "June 03, 2019 14:30:00",
+		"subject": "1:1",
+		"notes": ""
+	},
+	{
+		"date": "June 03, 2019 15:00:00",
+		"subject": "GUS Template Help",
 		"notes": ""
 	}
 ]
