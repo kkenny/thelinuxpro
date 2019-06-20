64d6d70480ca11ebf36d382b6122cc17fa550f5c
Kameron Kenny <kameron.kenny@salesforce.com>
2019-06-20 14:00:01 -0400

    testing

--- punch_list/eventManage.js
+++ punch_list/eventManage.js
@@ -410,5 +410,14 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	getJson();
 }
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
+
 
