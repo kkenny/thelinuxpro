12cd85718f5d3f3ebc0e73a3b3487fdaff5d89e1
Kameron Kenny <kameron.kenny@salesforce.com>
2019-06-25 10:36:28 -0400

    init rpr222

---
+++ rpr222/metadata.js
@@ -0,0 +1,16 @@
+var version = "0.14.031"
+var debug = false;
+var jsonUrl = 'https://api.myjson.com/bins/13hsch';
+
+var events, list, counter_diff, eventLength, isEventsArray;
+var currentObject, nextObject, notes;
+var currentDate, nextDate, currentStart, currentEnd;
+var nextStart, nextEnd, currentSubject, nextSubject;
+
+var dataLoad = false;
+var dataRequested = false;
+var initialized = false;
+var style = 'green';
+var eventStyle = "events";
+var array_counter = 0;
+
